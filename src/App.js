998246ab9863c7f56f75a460e5cc6aa50cfd2f02
Digitalworldlover <gm.gmbh.ch@gmail.com>
2020-11-04 13:20:38 +0300

    all files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import profileImage from './img/sel.jpg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> My name is Salahuddin Hissam</h1>
+      <h2>I am trying to create react build app.</h2>
+      <img src={profileImage} alt="Hello everyone!" />
+
     </div>
   );
 }
